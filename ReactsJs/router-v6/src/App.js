597092c027e1bbd7d9170e10338025f8ebaed1e2
xeeshanshani <53055692+xeeshanshani@users.noreply.github.com>
2022-12-05 23:10:47 +0500

    Route v6

---
+++ ReactsJs/router-v6/src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+//Import the pages
+
+import Page1 from "./Components/page1"
+import Page2 from "./Components/page2"
+import Page3 from "./Components/page3"
+
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<h1>Home Page</h1>} />
+          <Route exact path="page1" element={<Page1 />} />
+          <Route exact path="page2" element={<Page2 />} />
+          <Route exact path="page3" element={<Page3 />} />
+        </Routes>
+        <div className="list">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="page1">Page 1</Link></li>
+            <li><Link to="page2">Page 2</Link></li>
+            <li><Link to="page3">Page 3</Link></li>
+          </ul>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactsJs/router-v6/src/Components/page1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page1() {
+  return (
+    <div>
+       <h1>Page 1</h1>
+    </div>
+  )
+}
+
